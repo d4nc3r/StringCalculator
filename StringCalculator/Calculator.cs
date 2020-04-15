7abac640cf5b37511ec966b1e69e31e05310adfc
Jamie Mitch <7iny.d4nc3r@gmail.com>
2020-04-15 14:52:10 -0400

    Add project files.

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        internal int Calculate(string v)
+        {
+            return -1;
+        }
+    }
+}
